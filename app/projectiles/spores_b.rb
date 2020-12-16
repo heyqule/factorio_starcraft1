94a8fe857583292ba2e35f6d60c4f745aab2bb8a
heyqule <hq.jfax@gmail.com>
2020-12-15 20:38:08 -0500

    Change folder structure Add support for colorize masks Add Projectile support

--- app/projectiles/spores_b.rb
+++ app/projectiles/spores_b.rb
@@ -9,7 +9,7 @@ module ScGraphicConverter
         @image_postfix = '.bmp'
         @frame_count = 4
         @directions = 1
-        @output_file = 'spores_1'
+        @output_file = 'spores_2'
         @inner_path = 'bmps/projectile/spores_2'
         @use_17_rule = false
         @use_flip = false
